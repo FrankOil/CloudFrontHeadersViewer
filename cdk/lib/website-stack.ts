047cdb52be312e9547adf07ee3b6e8cf2d3fde49
Franciszek Olejnik <18384894+FrankOil@users.noreply.github.com>
2022-11-01 22:00:39 +0100

    ✨ (CDK/lib/website-stack.ts) Add a CloudFront distribution.

--- cdk/lib/website-stack.ts
+++ cdk/lib/website-stack.ts
@@ -1,4 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Distribution, HttpVersion, SecurityPolicyProtocol } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 import { BlockPublicAccess, Bucket, BucketEncryption, ObjectOwnership } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
@@ -6,11 +8,20 @@ export class WebsiteStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
-		const frontendBucket = new Bucket(this, 'FrontendBucket', {
+		const frontendBucket = new Bucket(this, "FrontendBucket", {
 			encryption: BucketEncryption.S3_MANAGED, // Required by OAI (Origin Access Identity).
 			enforceSSL: true,
 			objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
-			blockPublicAccess: BlockPublicAccess.BLOCK_ALL
+			blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+		});
+
+		new Distribution(this, "Distribution", {
+			defaultBehavior: {
+				origin: new S3Origin(frontendBucket),
+			},
+			defaultRootObject: "index.html",
+			httpVersion: HttpVersion.HTTP2_AND_3,
+			minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
 		});
 	}
 }
