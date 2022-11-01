e114ef551ffe3e2a6740f75f43ddab558557b219
Franciszek Olejnik <18384894+FrankOil@users.noreply.github.com>
2022-11-01 15:24:59 +0100

    ✨ (CDK/lib/website-stack.ts) Add a frontend bucket.

--- cdk/lib/website-stack.ts
+++ cdk/lib/website-stack.ts
@@ -1,8 +1,14 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class WebsiteStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
+
+		const frontendBucket = new Bucket(this, 'FrontendBucket', {
+			encryption: BucketEncryption.S3_MANAGED, // Required by OAI (Origin Access Identity).
+			enforceSSL: true,
+		});
 	}
 }
