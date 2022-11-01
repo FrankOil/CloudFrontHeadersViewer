6657b3c90571e164b3475a8c348296b08c940628
Franciszek Olejnik <18384894+FrankOil@users.noreply.github.com>
2022-11-01 15:26:20 +0100

    ✅ (CDK/test/cdk.test.ts) Test the frontend bucket.

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,17 +1,37 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import { App } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { WebsiteStack } from "../lib/website-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("WebsiteStack Created", () => {
+	const app = new App();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+	const stack = new WebsiteStack(app, "MyTestStack");
+
+	const template = Template.fromStack(stack);
+
+	template.hasResourceProperties("AWS::S3::Bucket", {
+		BucketEncryption: {
+			ServerSideEncryptionConfiguration: [
+				{
+					ServerSideEncryptionByDefault: {
+						SSEAlgorithm: "AES256",
+					},
+				},
+			],
+		},
+	});
+
+	template.hasResourceProperties("AWS::S3::BucketPolicy", {
+		PolicyDocument: {
+			Statement: [
+				{
+					Condition: {
+						Bool: {
+							"aws:SecureTransport": "false"
+						}
+					}
+				},
+			]
+		},
+	});
 });
