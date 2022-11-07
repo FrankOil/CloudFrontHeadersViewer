f3960986c2eb60137164c9f03b96159677fb52ba
Franciszek Olejnik <18384894+FrankOil@users.noreply.github.com>
2022-11-07 23:47:50 +0100

    ✨ (CDK/bin/cdk.ts) Add a type for process envs.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
-import 'source-map-support/register';
-import { WebsiteStack } from '../lib/website-stack';
+import { App } from "aws-cdk-lib";
+import "source-map-support/register";
+import { WebsiteStack } from "../lib/website-stack";
+
+declare global {
+	namespace NodeJS {
+		export interface ProcessEnv {
+			CDK_DEFAULT_ACCOUNT: string;
+			CDK_DEFAULT_REGION: string;
+		}
+	}
+}
 
 const app = new App();
 
-new WebsiteStack(app, 'WebsiteStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new WebsiteStack(app, "WebsiteStack", {
+	env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
