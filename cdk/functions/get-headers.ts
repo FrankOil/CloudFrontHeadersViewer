3aaf7c54a73cba8b6e919a36f1e8325332cf26a2
Franciszek Olejnik <18384894+FrankOil@users.noreply.github.com>
2022-11-15 21:54:49 +0100

    ✨ (CDK/Functions/GetHeaders) Validate request query parameters.

--- cdk/functions/get-headers.ts
+++ cdk/functions/get-headers.ts
@@ -1,76 +1,83 @@
 /* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 // Const and let statements are not supported in CloudFront Functions JavaScript runtime.
 // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/functions-javascript-runtime-features.html
 
 var querystring = require('querystring') as typeof import('querystring');
 
+function arrayEquals(a: string[], b: string[]) {
+	return a.length === b.length && a.every((value, index) => value === b[index]);
+}
+
+function redirect(host: string, queryParameters: Record<string, string>) {
+	return {
+		statusCode: 302,
+		statusDescription: 'Found',
+		headers: {
+			location: { value: `https://${host}/?${querystring.stringify(queryParameters)}` },
+		},
+	};
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function handler(event: AWSCloudFrontFunction.Event) {
 	var request = event.request;
+	var requestURI = request.uri;
+	var requestHeaders = request.headers;
+	var requestQueryParameters = request.querystring;
 
 	try {
-		var requestURI = request.uri;
+		// Do not modify asset requests
 
 		if (requestURI === '/favicon.svg' || requestURI.startsWith('/assets')) {
 			return request;
 		}
 
-		var requestHeaders = request.headers;
+		// Extract the host header
 
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		var requestURL = `https://${requestHeaders['host']!.value}`;
+		var hostHeader = requestHeaders['host']!.value;
 
-		var viewerAddress = requestHeaders['cloudfront-viewer-address']
-			? requestHeaders['cloudfront-viewer-address'].value
-			: '';
+		// Extract CloudFront HTTP headers
 
-		var viewerCountry = requestHeaders['cloudfront-viewer-country']
-			? requestHeaders['cloudfront-viewer-country'].value
-			: '';
+		var correctQueryParameters = {
+			ip_address: requestHeaders['cloudfront-viewer-address']!.value,
+			country: requestHeaders['cloudfront-viewer-country']!.value,
+			region: requestHeaders['cloudfront-viewer-country-region-name']!.value,
+			city: requestHeaders['cloudfront-viewer-city']!.value,
+			latitude: requestHeaders['cloudfront-viewer-latitude']!.value,
+			longitude: requestHeaders['cloudfront-viewer-longitude']!.value,
+			postal_code: requestHeaders['cloudfront-viewer-postal-code']!.value,
+			time_zone: requestHeaders['cloudfront-viewer-time-zone']!.value,
+		};
 
-		var viewerRegion = requestHeaders['cloudfront-viewer-country-region-name']
-			? requestHeaders['cloudfront-viewer-country-region-name'].value
-			: '';
-
-		var viewerCity = requestHeaders['cloudfront-viewer-city'] ? requestHeaders['cloudfront-viewer-city'].value : '';
-
-		var viewerLatitude = requestHeaders['cloudfront-viewer-latitude']
-			? requestHeaders['cloudfront-viewer-latitude'].value
-			: '';
-
-		var viewerLongitude = requestHeaders['cloudfront-viewer-longitude']
-			? requestHeaders['cloudfront-viewer-longitude'].value
-			: '';
-
-		var viewerPostalCode = requestHeaders['cloudfront-viewer-postal-code']
-			? requestHeaders['cloudfront-viewer-postal-code'].value
-			: '';
-
-		var viewerTimeZone = requestHeaders['cloudfront-viewer-time-zone']
-			? requestHeaders['cloudfront-viewer-time-zone'].value
-			: '';
+		// Redirect users to the index.html
 
 		if (requestURI !== '/') {
-			var newQueryString = querystring.stringify({
-				ip_address: viewerAddress,
-				country: viewerCountry,
-				region: viewerRegion,
-				city: viewerCity,
-				latitude: viewerLatitude,
-				longitude: viewerLongitude,
-				postal_code: viewerPostalCode,
-				time_zone: viewerTimeZone,
-			});
+			return redirect(hostHeader, correctQueryParameters);
+		}
 
-			return {
-				statusCode: 302,
-				statusDescription: 'Found',
-				headers: {
-					location: { value: `${requestURL}/?${newQueryString}` },
-				},
-			};
+		// Redirect if the request does not contain all the required query parameters or contains additional ones.
+
+		var correctQueryParameterNames = Object.keys(correctQueryParameters).sort();
+		var requestQueryParameterNames = Object.keys(requestQueryParameters).sort();
+
+		if (!arrayEquals(correctQueryParameterNames, requestQueryParameterNames)) {
+			return redirect(hostHeader, correctQueryParameters);
+		}
+
+		// Redirect if the request query parameters are incorrect
+
+		var correctQueryParameterValues = correctQueryParameterNames.map(
+			(name) => correctQueryParameters[name as keyof typeof correctQueryParameters],
+		);
+		var requestQueryParameterValues = requestQueryParameterNames.map((name) =>
+			decodeURIComponent(requestQueryParameters[name]!.value),
+		);
+
+		if (!arrayEquals(correctQueryParameterValues, requestQueryParameterValues)) {
+			return redirect(hostHeader, correctQueryParameters);
 		}
 
 		return request;
